5ee0e4be9e6e2182b0b48dc28df27ca0eb8c3e02
Jesus Yegres <jesusyegrestx@gmail.com>
2019-06-13 12:33:24 -0500

    START PROJECT WEATHER APP

---
+++ weather-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import WeatherLocation from './components/weatherLocation';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+        Weather App del clima
+        <WeatherLocation></WeatherLocation>
+    </div>
+  );
+}
+
+export default App;
