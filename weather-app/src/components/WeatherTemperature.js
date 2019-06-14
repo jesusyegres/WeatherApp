6d09359eb3e03221f95dac566d0816e2f01f9f66
Jesus Yegres <jesusyegrestx@gmail.com>
2019-06-14 15:08:34 -0500

    Add icons to weather app 9%

--- weather-app/src/components/WeatherTemperature.js
+++ weather-app/src/components/WeatherTemperature.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import WeatherIcons from 'react-weathericons';
 
-const WeatherTemperature = () => {
+const icons = {
+    sunny: "day-sunny",
+    fog: "day-fog",
+}
+
+const getWeatherIcon = weatherState => {
+    const icon = icons[weatherState];
+    
+    if (icon)
+        return <WeatherIcons name={icon} size="2x"></WeatherIcons>
+    else 
+    return <WeatherIcons name={"day-fog"} size="2x"></WeatherIcons>
+}
+
+const WeatherTemperature = ({temperature, weatherState}) => {
     return (
         <div>
-            <span> 25 Grados</span>
+            {
+                getWeatherIcon(weatherState)
+            }
+            <span>{`${temperature} Grados`}</span>
         </div>
     );
 };
