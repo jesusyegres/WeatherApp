87f119f48ce13cb4da45422e65058848b347986b
Jesus Yegres <jesusyegrestx@gmail.com>
2019-06-29 14:13:16 -0500

    implementation middleware

--- weather-app/src/actions/index.js
+++ weather-app/src/actions/index.js
@@ -1,2 +1,31 @@
+import { api_key, url_base_forecast } from '../constants/api_url';
+import transforForecast from './../services/transforForecast';
 export const SET_CITY = 'SET-CITY';
+export const SET_FORECAST_DATA = 'SET_FORECAST_DATA';
 export const setCity = payload => ({type:SET_CITY,payload});
+
+export const setForecastData = payload =>({ type: SET_FORECAST_DATA, payload });
+
+export const setSelectedCity = payload => {
+
+
+        return dispatch =>{
+
+            const url_forecast = `${url_base_forecast}?q=${payload}&appid=${api_key}`;
+            
+            // activar en el estado un indicador de busqueda de datos
+            dispatch(setCity(payload));
+            return fetch(url_forecast).then(
+                data => (data.json())
+            ).then(
+                weather_data => { 
+                    const forecastData = transforForecast(weather_data);
+                    console.log(forecastData);
+                    
+                    //modificar el estado con el resultado de la promise (fetch)
+                    dispatch(setForecastData({city:payload,forecastData}))
+                }
+            )
+
+        };
+};
--- weather-app/src/store/index.js
+++ weather-app/src/store/index.js
@@ -1,11 +1,14 @@
-import { createStore} from 'redux';
+import { createStore, applyMiddleware, compose} from 'redux';
 import {city} from './../reducers/city';
+import thunk from 'redux-thunk';
 
 const initialState = {
     city: 'Buenos Aires,ar'
 };
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__ || compose;
 
-export const store = createStore(city, initialState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+export const store = createStore(city, initialState, composeEnhancers(applyMiddleware(thunk)));
+
 
  
